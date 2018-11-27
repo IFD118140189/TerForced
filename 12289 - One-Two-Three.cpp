e1ce45dee190b6294157f47a9fc7e4dfe6af34fe
IFD118140189 <45229043+IFD118140189@users.noreply.github.com>
2018-11-27 11:32:18 +0700

    Add files via upload

---
+++ 12289 - One-Two-Three.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string a; int b;
+	string e="one";
+	string f="two";
+	string g="three";
+	cin>>b;
+	
+	for(int i=1; i<=b; i++){
+		int c=0;
+		cin>>a;
+		c=a.length();
+		int d=0;
+		for(int j=0; j<c; j++){
+			if(i==1){
+				if(a[0]==e[0]||a[1]==e[1]||a[2]==e[2]){
+					d++;
+				}
+			} else if(i==2){
+				if(a[0]==f[0]||a[1]==f[1]||a[2]==f[2])
+				d++;	
+			}
+		}
+		if(d==2||d==3||c>3)
+		cout<<i<<endl;
+	}
+}
